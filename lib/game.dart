4b099c6811653b40aec91403190bd1378f2bcf46
raghull15 <rbraghul1508@gmail.com>
2025-05-31 21:20:14 +0530

    31

--- lib/game.dart
+++ lib/game.dart
@@ -5,15 +5,18 @@ class MyGame extends FlameGame {
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    
-    final paint1 = Paint()..color = const Color.fromARGB(255, 255, 81, 0); 
-    final paint2 = Paint()..color = const Color.fromARGB(255, 0, 128, 255); 
-    final paint3 = Paint()..color = const Color.fromARGB(255, 0, 200, 0);   
+
+    final paint1 = Paint();
+    paint1.color = const Color.fromARGB(255, 255, 81, 0);
+
+    final paint2 = Paint();
+    paint2.color = const Color.fromARGB(255, 0, 128, 255);
+
+    final paint3 = Paint();
+    paint3.color = const Color.fromARGB(255, 0, 200, 0);
 
     canvas.drawRect(const Rect.fromLTWH(10, 50, 100, 100), paint1);
-
     canvas.drawRect(const Rect.fromLTWH(150, 50, 80, 120), paint2);
-
     canvas.drawRect(const Rect.fromLTWH(250, 200, 120, 80), paint3);
   }
 }
